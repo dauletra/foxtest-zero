f1976facc6144d0a6e396a6331bc8b732b262237
Daulet Rakhmankul <daulet.rakhmankul@gmail.com>
2018-04-17 21:01:12 +0600

    Get test from foxtestconsole github repo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="container border">
 
-
     <div class="border mb-4 p-1">
-      <form v-on:submit.prevent class="form-inline">
-        <div class="form-group">
-          <input v-on:change="onChangeHandler" type="file" class="form-control-file from-control" id="file">
+      Студенттерге тест жаттауға көмектесетін сайт
+    </div>
+
+    <div class="mb-4 p-2 border rounded">
+      <form v-on:submit.prevent>
+
+        <div class="">
+          Код жазу арқылы серверден жүктеу
+          <div class="input-group" style="max-width: 300px">
+            <input v-model.trim="code" type="text" class="form-control" placeholder="Код">
+            <div class="input-group-append">
+              <button v-bind:disabled="downloading" v-on:click="download" type="submit" class="btn">Жүктеу</button>
+            </div>
+          </div>
+          <span class="text-warning small">{{codeWarning}}</span>
+          <span class="text-success small">{{codeSuccess}}</span>
         </div>
-        <div class="form-group">
-          <button type="submit" class="btn btn-primary">Жүктеу</button>
+
+        <div class="hr-sect">немесе</div>
+
+        <div class="">
+          Компьютер немесе телефон жадынан жүктеу
+          <div class="">
+            <input v-on:change="onChangeHandler" type="file" class="form-control-file from-control" id="file">
+          </div>
+          <span class="text-warning small">{{fileWarning}}</span>
+          <span class="text-success small">{{fileSuccess}}</span>
         </div>
+
       </form>
     </div>
 
@@ -19,30 +40,68 @@
 </template>
 
 <script>
+  import axios from 'axios'
   import Trainer from './components/Trainer'
 
   export default {
     name: 'app',
     data () {
       return {
-        quizBank: {}
+        quizBank: {},
+
+        code: '',
+        codeWarning: '',
+        codeSuccess: '',
+        downloading: false,
+
+        fileWarning: '',
+        fileSuccess: ''
       }
     },
     components: {
       Trainer
     },
     methods: {
-      hello() {
-        console.log()
+      download() {
+        this.code = this.code.toLowerCase();
+        if (!this.code.length > 0)
+          return;
+        let url = `https://dauletra.github.io/foxtestconsole/examples/${this.code}.json`;
+        console.log(`Generated url: ${url}`);
+        // todo adblock блокирует ссылку
+        this.downloading = true;
+        axios.get(url, {
+          onDownloadProgress: event => {
+            // this.progress = event.loaded; Math.round((event.loaded*100) / event.total); // event.total == 0
+          }
+        })
+          .then(response => {
+            this.quizBank = response.data;
+            this.codeWarning = '';
+            this.codeSuccess = 'Сәтті жүктелді';
+            this.downloading = false;
+          })
+          .catch(ex => {
+            console.log('Файлды серверден жүктеу кезінде қате кетті: ', ex);
+            this.codeWarning = 'Код дұрыс емес';
+            this.codeSuccess = '';
+            this.downloading = false;
+          });
       },
       onChangeHandler(evt) {
         let files = evt.target.files;
         let file = files[0];
         let reader = new FileReader();
         reader.onload = (event) => {
-          this.quizBank = JSON.parse(event.target.result);
-          console.log('QuizBank successfilly parsed: ' + this.quizBank.name);
-          console.log(this.quizBank);
+          try {
+            this.quizBank = JSON.parse(event.target.result);
+            this.fileWarning = '';
+            this.fileSuccess = 'Сәтті жүктелді'
+          } catch (err) {
+            console.log('Файл жүктеу кезінде қате кетті', err);
+            this.fileWarning = 'Файл форматы дұрыс емес';
+            this.fileSuccess = ''
+          }
         };
         reader.readAsText(file);
       }
@@ -50,3 +109,22 @@
   }
 </script>
 
+<style>
+  .hr-sect {
+    display: flex;
+    flex-basis: 100%;
+    align-items: center;
+    color: rgba(0, 0, 0, 0.35);
+    margin: 8px 0px;
+  }
+  .hr-sect::before,
+  .hr-sect::after {
+    content: "";
+    flex-grow: 1;
+    background: rgba(0, 0, 0, 0.35);
+    height: 1px;
+    font-size: 0px;
+    line-height: 0px;
+    margin: 0px 8px;
+  }
+</style>
